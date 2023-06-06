200a66b2116a4ee240112fe10c358d5cb573127a
francohilt <ivanfranco636@gmail.com>
2023-06-05 22:55:14 -0300

    update

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <body>
     <h1>Mapa de Regiones Geográficas de Argentina</h1>
     <div id="map"></div>
+    <div id="coordinates"></div>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script>
         function initMap() {
@@ -27,24 +28,77 @@
                 maxZoom: 18
             }).addTo(map);
             var regions = [
-                { name: 'Patagonia', coordinates: [[-41.0, -73.0], [-41.0, -63.0],  [-56.0, -63.0], [-56.0, -73.0]], color: 'blue', link: 'https://example.com/patagonia' },
+                 // { name: 'Argentina', coordinates: [
+                //     // Punto 1: Extremo noroeste
+                //     [-21.7812, -73.9502],
+
+                //     // Punto 2: Extremo noreste
+                //     [-21.7812, -53.9648],
+
+                //     // Punto 3: Extremo sureste
+                //     [-54.8860, -53.9648],
+
+                //     // Punto 4: Extremo suroeste
+                //     [-54.8860, -73.9502]],
+                //     color: 'green', link: 'https://example.com/patagonia' },
+
+                { name: 'Patagonia', coordinates: [
+                    [-38.0, -72.0], 
+                    [-38.0, -68.0], 
+                    [-39.5, -68.0], 
+                    [-39.5, -62.0],
+                    [-55.0, -62.0], 
+                    [-55.0, -72.0],  
+                ], 
+                    color: 'rgba(0, 51, 102, 0.8)', link: 'https://example.com/patagonia' },
+                
                 { name: 'Cuyo', coordinates: [  
-                    [-29.0, -72.0],
-                    [-29.0, -64.0],
-                    [-37.0, -64.0],
-                    [-37.0, -72.0]
-                ], color: 'green', link: 'https://example.com/cuyo' },
-                { name: 'Llanura Pampeana', coordinates: [
-                   [-33.0, -65.0],
-                   [-33.0, -59.0],
-                   [-41.0, -59.0],
-                   [-41.0, -65.0]
-                ], color: 'orange', link: 'https://example.com/pampeana' },
+                    [-38.0, -71.0], 
+                    [-38.0, -68.0], 
+                    [-27.0, -68.0], 
+                    [-27.0, -71.0], 
+                ], color: 'rgba(102, 0, 0, 0.8)', link: 'https://example.com/cuyo' },
+                
+                { name: 'Sierras Pampeanas', coordinates: [  
+                    [-34.0, -68.0], 
+                    [-34.0, -63.8], 
+                    [-27.0, -63.8], 
+                    [-27.0, -68.0], 
+                ], color: 'rgba(102, 51, 0, 0.8)', link: 'https://example.com/sierrasPampeanas' },
+
+                { name: 'Llanura Pampeana', coordinates: [  
+                    [-34.0, -68.0], 
+                    [-34.0, -63.8], 
+                    [-31.0, -63.8], 
+                    [-31.0, -57.0],
+                    [-39.5, -57.0],
+                    [-39.5, -68.0],
+                ], color: 'rgba(255, 102, 0, 0.8)', link: 'https://example.com/llanuraPampeana' },
+
+                { name: 'Noroeste', coordinates: [  
+                    [-27, -68.8],
+                    [-27, -63.8], 
+                    [-21.7, -63.8],
+                    [-21.7, -68.8],
+                ], color: 'rgba(204, 0, 0, 0.8)', link: 'https://example.com/noroeste' },
+
+                { name: 'Llanura Subtropical', coordinates: [  
+                    [-31.0, -63.8], 
+                    [-31.0, -57.0],
+                    [-21.7, -57.0],
+                    [-21.7, -63.8],
+                ], color: 'rgba(255, 102, 153, 0.8)', link: 'https://example.com/llanuraSubtropical' },
+
             ];
             for (var i = 0; i < regions.length; i++) {
                 var region = regions[i];
-                var polygon = L.polygon(region.coordinates, { color: region.color }).addTo(map);
+                var polygon = L.polygon(region.coordinates, {color: 'black', fillColor: region.color, fillOpacity: 0.6 }).addTo(map);
                 polygon.bindPopup('<strong>' + region.name + '</strong><br><a href="' + region.link + '">Más información</a>');
+
+                polygon.on('click', function (event) {
+                    var latLng = event.latlng;
+                    document.getElementById('coordinates').innerHTML = 'Latitud: ' + latLng.lat.toFixed(4) + ', Longitud: ' + latLng.lng.toFixed(4);
+                });
             }
         }
     </script>
